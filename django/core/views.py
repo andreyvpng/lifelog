6fe326c549e2ebe4511b2698a8a0c4c1e7eb696e
andreyvpng <andrewvarfolomeev@protonmail.com>
2018-09-24 16:18:58 +1000

    created RecordListView and urls for this

--- django/core/views.py
+++ django/core/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.views.generic import ListView
+from django.contrib.auth.mixins import LoginRequiredMixin
 
-# Create your views here.
+from core.models import Record
+
+
+class RecordListView(LoginRequiredMixin, ListView):
+
+    def get_queryset(self):
+        qs = Record.objects.filter(
+            action__user=self.request.user
+        )
+        return qs
