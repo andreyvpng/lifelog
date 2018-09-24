9a41cef6b2afeb2b4f88085993a93dba41347991
andreyvpng <andrewvarfolomeev@protonmail.com>
2018-09-24 18:29:33 +1000

    created ActionListView, ActionCreateView and added urls for this

--- django/core/urls.py
+++ django/core/urls.py
@@ -7,4 +7,10 @@ urlpatterns = [
     path('records',
          views.RecordListView.as_view(),
          name='record-list'),
+    path('actions',
+         views.ActionListView.as_view(),
+         name='action-list'),
+    path('action-create',
+         views.ActionCreateView.as_view(),
+         name='action-create'),
 ]
--- django/core/views.py
+++ django/core/views.py
@@ -1,7 +1,8 @@
-from django.views.generic import ListView
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.urls import reverse
+from django.views.generic import CreateView, ListView
 
-from core.models import Record
+from core.models import Action, Record
 
 
 class RecordListView(LoginRequiredMixin, ListView):
@@ -11,3 +12,28 @@ class RecordListView(LoginRequiredMixin, ListView):
             action__user=self.request.user
         )
         return qs
+
+
+class ActionListView(LoginRequiredMixin, ListView):
+
+    def get_queryset(self):
+        qs = Action.objects.filter(
+            user=self.request.user
+        )
+        return qs
+
+
+class ActionCreateView(LoginRequiredMixin, CreateView):
+    model = Action
+    fields = ('text', 'unit')
+
+    def form_valid(self, form):
+        self.object = form.save(commit=False)
+        self.object.user = self.request.user
+
+        self.object.save()
+
+        return super().form_valid(form)
+
+    def get_success_url(self):
+        return reverse('core:record-list')
