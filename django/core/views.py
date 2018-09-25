634c849d001c02990070dba1fc3625715d3a00f6
andreyvpng <andrewvarfolomeev@protonmail.com>
2018-09-25 17:15:20 +1000

    some changes

--- django/core/views.py
+++ django/core/views.py
@@ -1,7 +1,7 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.urls import reverse
 from django.http.response import HttpResponseBadRequest
-from django.views.generic import CreateView, ListView
+from django.views.generic import CreateView, ListView, UpdateView
 
 from core.models import Action, Record
 from core.forms import RecordCreate
@@ -24,6 +24,29 @@ class RecordListView(LoginRequiredMixin, ListView):
             action__user=self.request.user
         )
         return qs
+
+
+class RecordCreateView(LoginRequiredMixin, CreateView):
+    model = Record
+    form_class = RecordCreate
+
+    def form_valid(self, form):
+        self.object = form.save(commit=False)
+
+        if self.object.action.user != self.request.user:
+            return HttpResponseBadRequest()
+
+        self.object.save()
+
+        return super().form_valid(form)
+
+    def get_form_kwargs(self):
+        kwargs = super(RecordCreateView, self).get_form_kwargs()
+        kwargs['user'] = self.request.user
+        return kwargs
+
+    def get_success_url(self):
+        return reverse('core:record-list')
 
 
 class ActionListView(LoginRequiredMixin, ListView):
@@ -49,26 +72,3 @@ class ActionCreateView(LoginRequiredMixin, CreateView):
 
     def get_success_url(self):
         return reverse('core:action-list')
-
-
-class RecordCreateView(LoginRequiredMixin, CreateView):
-    model = Record
-    form_class = RecordCreate
-
-    def form_valid(self, form):
-        self.object = form.save(commit=False)
-
-        if self.object.action.user != self.request.user:
-            return HttpResponseBadRequest()
-
-        self.object.save()
-
-        return super().form_valid(form)
-
-    def get_form_kwargs(self):
-        kwargs = super(RecordCreateView, self).get_form_kwargs()
-        kwargs['user'] = self.request.user
-        return kwargs
-
-    def get_success_url(self):
-        return reverse('core:record-list')
