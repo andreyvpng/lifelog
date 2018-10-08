365951588070d5c5a428367777fc39f393ea1c57
andreyvpng <andrewvarfolomeev@protonmail.com>
2018-10-08 17:43:16 +1000

    show all actions in dashboard

--- django/dashboard/models.py
+++ django/dashboard/models.py
@@ -1,5 +1,4 @@
 from django.db import models
-from django.db.models.aggregates import Sum
 
 
 class ActionDashboardManager(models.Manager):
@@ -7,11 +6,19 @@ class ActionDashboardManager(models.Manager):
     def get(self, user, year, month, day):
         qs = self.get_queryset()
         qs = qs.filter(user=user)
-        qs = qs.filter(
-            records__created__day=day,
-            records__created__month=month,
-            records__created__year=year
-        )
+
         qs = qs.annotate(
-            record_sum=Sum('records__value'))
+            record_sum=models.aggregates.Sum(
+                models.Case(
+                    models.When(
+                        records__created__day=day,
+                        records__created__month=month,
+                        records__created__year=year,
+                        then=models.F('records__value')
+                    ),
+                    output_field=models.PositiveIntegerField(),
+                    default=0
+                )
+            ),
+        )
         return qs
