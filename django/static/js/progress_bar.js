f0b4c603398a28b021857e481929b07492f2812a
andreyvpng <andrewvarfolomeev@protonmail.com>
2018-10-12 13:02:39 +1000

    delete debug logs

--- django/static/js/progress_bar.js
+++ django/static/js/progress_bar.js
@@ -4,12 +4,9 @@ $(".card > .progress > .progress-bar").each(function( index ) {
       record_sum = $( this ).attr('record_sum'),
       goal_daily_value = $( this ).attr('goal_daily_value');
 
-  console.log(record_sum, goal_daily_value)
-
   // Is a goal created for this action?
   if (goal_daily_value != "") {
     percent = parseInt(record_sum) * 100 / parseInt(goal_daily_value);
-  console.log(percent)
 
     if (percent > 100) {
       percent = 100;
