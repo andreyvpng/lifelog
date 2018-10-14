3cd103f90f5fc2f8f97f0266b69a0c74039ec044
andreyvpng <andrewvarfolomeev@protonmail.com>
2018-10-15 00:37:04 +1000

    add test for ActionDeleteView

--- django/core/tests/test_views.py
+++ django/core/tests/test_views.py
@@ -37,7 +37,7 @@ class ActionCreateViewTest(TestCase):
         resp = self.client.post('/action-create', self.object)
         object = Action.objects.filter(user=self.user)
 
-        # Check our user is create action
+        # Check that the user has created our action
         self.assertTrue(object)
 
         self.assertEqual(resp.status_code, 302)
@@ -47,3 +47,33 @@ class ActionCreateViewTest(TestCase):
         resp = self.client.post('/action-create', self.object)
         self.assertEqual(resp.status_code, 302)
         self.assertRedirects(resp, '/user/login?next=/action-create')
+
+
+class ActionDeleteViewTest(TestCase):
+
+    def setUp(self):
+        self.user = UserFactory(username='test', password='12345')
+        self.other_user = UserFactory(username='test1', password='12345')
+        self.action = ActionFactory(user=self.user)
+        self.url = '/action-delete/{}'.format(self.action.id)
+
+    def test_delete_action_by_user(self):
+        resp = self.client.login(username='test', password='12345')
+        resp = self.client.post(self.url)
+        object = Action.objects.filter(user=self.user)
+
+        # Check that the user has deleted our action
+        self.assertFalse(object)
+
+        self.assertEqual(resp.status_code, 302)
+        self.assertRedirects(resp, '/dashboard/')
+
+    def test_delete_action_by_other_user(self):
+        resp = self.client.login(username='test1', password='12345')
+        resp = self.client.post(self.url)
+        object = Action.objects.filter(user=self.user)
+
+        # Check that the other user does not remove the action
+        self.assertTrue(object)
+
+        self.assertEqual(resp.status_code, 403)
