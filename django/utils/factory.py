60ed11e9404fff92d018de9af0b5e9557978ff6a
andreyvpng <andrewvarfolomeev@protonmail.com>
2018-10-14 19:46:22 +1000

    fix UserFactory

--- django/utils/factory.py
+++ django/utils/factory.py
@@ -11,12 +11,17 @@ class CreateFactoryMixin(factory.DjangoModelFactory):
         return manager.create(*args, **kwargs)
 
 
-class UserFactory(CreateFactoryMixin, factory.DjangoModelFactory):
+class UserFactory(factory.DjangoModelFactory):
     username = factory.Sequence(lambda n: 'user %d' % n)
     password = 'unittest'
 
     class Meta:
         model = settings.AUTH_USER_MODEL
+
+    @classmethod
+    def _create(cls, model_class, *args, **kwargs):
+        manager = cls._get_manager(model_class)
+        return manager.create_user(*args, **kwargs)
 
 
 class ActionFactory(CreateFactoryMixin, factory.DjangoModelFactory):
