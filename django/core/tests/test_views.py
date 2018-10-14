79831c84da356288d518eeeb58f0d2ace2fde5ba
andreyvpng <andrewvarfolomeev@protonmail.com>
2018-10-15 00:13:10 +1000

    create test for ActionCreateView

--- django/core/tests/test_views.py
+++ django/core/tests/test_views.py
@@ -1,11 +1,11 @@
-from django.test import TestCase, RequestFactory
-from utils.factory import UserFactory
+from core.models import Action
+from django.test import TestCase
+from utils.factory import UserFactory, ActionFactory
 
 
 class WelcomeViewTest(TestCase):
 
     def setUp(self):
-        self.factory = RequestFactory()
         self.user = UserFactory(username='test', password='12345')
 
     def test_not_authorized_user(self):
@@ -22,3 +22,28 @@ class WelcomeViewTest(TestCase):
         resp = self.client.get('/')
         self.assertEqual(resp.status_code, 302)
         self.assertRedirects(resp, '/dashboard/')
+
+
+class ActionCreateViewTest(TestCase):
+
+    def setUp(self):
+        self.user = UserFactory(username='test', password='12345')
+        self.object = {'text': 'Book Reading',
+                       'color': '1',
+                       'unit': 'pages'}
+
+    def test_authorized_user(self):
+        resp = self.client.login(username='test', password='12345')
+        resp = self.client.post('/action-create', self.object)
+        object = Action.objects.filter(user=self.user)
+
+        # Check our user is create action
+        self.assertTrue(object)
+
+        self.assertEqual(resp.status_code, 302)
+        self.assertRedirects(resp, '/dashboard/')
+
+    def test_not_authorized_user(self):
+        resp = self.client.post('/action-create', self.object)
+        self.assertEqual(resp.status_code, 302)
+        self.assertRedirects(resp, '/user/login?next=/action-create')
