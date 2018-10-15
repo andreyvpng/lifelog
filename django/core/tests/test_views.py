6e0a4c4ddf2a812ccb1a830bbabd8a36c06cfeb1
andreyvpng <andrewvarfolomeev@protonmail.com>
2018-10-15 12:53:32 +1000

    add test for ActionUpdateView

--- django/core/tests/test_views.py
+++ django/core/tests/test_views.py
@@ -77,3 +77,47 @@ class ActionDeleteViewTest(TestCase):
         self.assertTrue(object)
 
         self.assertEqual(resp.status_code, 403)
+
+
+class ActionUpdateViewTest(TestCase):
+
+    def setUp(self):
+        self.user = UserFactory(username='test', password='12345')
+        self.other_user = UserFactory(username='test1', password='12345')
+
+        self.object = {'text': 'Action',
+                       'color': 2,
+                       'unit': 'unit'}
+
+        self.action = ActionFactory(user=self.user, **self.object)
+
+        self.update_object = {'text': 'Book Reading',
+                              'color': 1,
+                              'unit': 'pages'}
+
+        self.url = '/action-update/{}'.format(self.action.id)
+
+    def test_update_action_by_user(self):
+        resp = self.client.login(username='test', password='12345')
+        resp = self.client.post(self.url, self.update_object)
+        object = Action.objects.filter(user=self.user)[0]
+
+        # Check that the user has update our action
+        self.assertEqual(object.text, self.update_object['text'])
+        self.assertEqual(object.color, self.update_object['color'])
+        self.assertEqual(object.unit, self.update_object['unit'])
+
+        self.assertEqual(resp.status_code, 302)
+        self.assertRedirects(resp, '/dashboard/')
+
+    def test_update_action_by_other_user(self):
+        resp = self.client.login(username='test1', password='12345')
+        resp = self.client.post(self.url, self.update_object)
+        object = Action.objects.filter(user=self.user)[0]
+
+        # Check that the user does not have updated our action
+        self.assertEqual(object.text, self.object['text'])
+        self.assertEqual(object.color, self.object['color'])
+        self.assertEqual(object.unit, self.object['unit'])
+
+        self.assertEqual(resp.status_code, 403)
