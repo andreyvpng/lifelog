fcd8c05dea213426e1ee2a44bd24d94a6b5e663f
andreyvpng <andrewvarfolomeev@protonmail.com>
2018-10-20 19:05:53 +1000

    enable token authorization

--- django/config/common_settings.py
+++ django/config/common_settings.py
@@ -162,3 +162,13 @@ CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 # Auth
 AUTH_USER_MODEL = 'user.User'
+
+
+# REST
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.TokenAuthentication',
+    ),
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.IsAuthenticated', )
+}
